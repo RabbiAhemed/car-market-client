ea9a4cf9812bebdf8b10c833ab5c5978d408af8b
Rabbi Ahmed Limon <rabbiahemed>
2022-11-26 00:15:36 +0600

    banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="my-5 text-center">
+      <img
+        className="img-fluid"
+        src="https://i.ibb.co/wNXRLzn/banner.jpg"
+        alt="banner"
+        border="0"
+      ></img>
+    </div>
+  );
+};
+
+export default Banner;
