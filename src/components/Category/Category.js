cc2784df138553c227c9c3b9b5da9494519fe375
Rabbi Ahmed Limon <rabbiahemed>
2022-11-27 15:47:39 +0600

    category card

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 import "./Category.css";
 const Category = ({ category }) => {
   const { _id, category_id, category_name, image } = category;
-  return <Link to={`/category/${_id}`}></Link>;
+  return (
+    <Link to={`/category/${_id}`}>
+      <div className="category-card border rounded py-4">
+        <img className="category_image" src={image} alt="category_image" />
+        <h4 className="mx-2 fw-bold ">{category_name}</h4>
+      </div>
+    </Link>
+  );
 };
 
 export default Category;
